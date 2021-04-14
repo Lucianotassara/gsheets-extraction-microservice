fa992dd1c8e3b8b914aa3f7a8bef765422acd746
Luciano Tassara <lucianotassara@gmail.com>
2021-04-13 22:59:41 -0300

    initial version

---
+++ controller/index.js
@@ -0,0 +1,5 @@
+import gSheetController from './gsheet.controller'
+
+export {
+    gSheetController
+};
---
+++ index.js
@@ -0,0 +1,45 @@
+require('dotenv').config()
+import express from 'express';
+import bodyParser from 'body-parser';
+import morgan from 'morgan';
+import cors from 'cors';
+import helmet from 'helmet';
+
+import { gSheetController } from './controller' 
+
+const app = express();
+
+app.use(cors());
+app.use(helmet());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(morgan('tiny'));
+
+// API
+app.use(gSheetController);
+
+function notFound(req, res, next) {
+  const error = new Error(`Not Found - ${req.originalUrl}`);
+  error.status = 404;
+  next(error);
+}
+
+// eslint-disable-next-line
+function errorHandler(err, req, res, next) {
+  const status = err.status || 500;
+  res.status(status);
+  res.json({
+    status,
+    error: err.message,
+  });
+}
+
+app.use(notFound);
+app.use(errorHandler);
+
+const  gSheetExtractorApi = process.env.GSHEET_API_PORT || 3006;
+
+app.listen(gSheetExtractorApi, () => {
+  console.log(`Started successfully server at port ${gSheetExtractorApi}`);
+  
+});
