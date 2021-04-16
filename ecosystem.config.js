73ee0b01b71e8a87fa4c5bd925f3a7cb3c0f9b6a
Luciano Tassara <lucianotassara@gmail.com>
2021-04-16 17:17:40 -0300

    deploy test

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     apps : [{
-      name: 'rvc-api',
-      script: './rvcApi.js',
+      name: 'gsheet-extractor',
+      script: './index.js',
   
       // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
       args: '--update-env',
